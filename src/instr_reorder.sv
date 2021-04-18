a83cc33aac5ce8de0dfc1b9210278d87576d688c
ChatPion <chatpion@gmail.com>
2021-04-18 23:25:30 +0200

    reorder_buf: fix fetch requests after a flush

--- src/instr_reorder.sv
+++ src/instr_reorder.sv
@@ -26,22 +26,15 @@ module instr_reorder (
 		logic 				is_ctrl_flow;
 	} issue_n, issue_q;
 
+	logic has_flushed_n, has_flushed_q;
+
 	always_comb begin
 
-		logic swap;
-		swap = issue_entry_valid_i
-			& ((issue_q.sbe.fu == ariane_pkg::STORE) | (issue_q.sbe.fu == ariane_pkg::LOAD))
-			& (issue_entry_i.fu != ariane_pkg::CTRL_FLOW)
-			& (issue_entry_i.fu != ariane_pkg::STORE)
-			& (issue_entry_i.fu != ariane_pkg::LOAD)
-			& (!lsu_ready_i)
-			& ((issue_entry_i.rs1 != issue_q.sbe.rd)|(issue_q.sbe.fu == ariane_pkg::STORE))
-			& ((issue_entry_i.rs2 != issue_q.sbe.rd)|(issue_q.sbe.fu == ariane_pkg::STORE))
-			& (issue_entry_i.rd != issue_q.sbe.rs1)
-			& ((issue_entry_i.rd != issue_q.sbe.rs2)|(issue_q.sbe.fu == ariane_pkg::LOAD))
-			& ((issue_entry_i.rd != issue_q.sbe.rd)|(issue_q.sbe.fu == ariane_pkg::STORE));
-
 		issue_n = issue_q;
+		has_flushed_n = has_flushed_q;
+
+		if (issue_entry_valid_i)
+			has_flushed_n = 0;
 
 		if (!issue_q.ie_valid) begin
 			issue_entry_o = issue_entry_i;
@@ -57,35 +50,40 @@ module instr_reorder (
 		end else begin
 			issue_instr_ack_o = issue_instr_ack_i;
 
-			if (swap) begin
-				issue_entry_o = issue_entry_i;
-				issue_entry_valid_o = issue_entry_valid_i;
-				is_ctrl_flow_o = is_ctrl_flow_i;
+			issue_entry_o = issue_q.sbe;
+			issue_entry_valid_o = issue_q.ie_valid;
+			is_ctrl_flow_o = issue_q.is_ctrl_flow;
 
-			end else begin
-				issue_entry_o = issue_q.sbe;
-				issue_entry_valid_o = issue_q.ie_valid;
-				is_ctrl_flow_o = issue_q.is_ctrl_flow;
-
-				if (issue_instr_ack_i) begin
-					issue_n.sbe = issue_entry_i;
-					issue_n.ie_valid = issue_entry_valid_i;
-					issue_n.is_ctrl_flow = is_ctrl_flow_i;
-				end
+			if (issue_instr_ack_i) begin
+				issue_n.sbe = issue_entry_i;
+				issue_n.ie_valid = issue_entry_valid_i;
+				issue_n.is_ctrl_flow = is_ctrl_flow_i;
 			end
 		end
+
+		if (has_flushed_n & has_flushed_q) begin
+			issue_instr_ack_o = 0;
+		end
 		
-		
+/*
+		issue_entry_o = issue_entry_i;
+		issue_entry_valid_o = issue_entry_valid_i;
+		is_ctrl_flow_o = is_ctrl_flow_i;
+		issue_instr_ack_o = issue_instr_ack_i;
+*/
 		if (flush_i) begin
 			issue_n = '0;
+			has_flushed_n = '1;
 		end
 	end
 
 	always_ff @(posedge clk_i or negedge rst_ni) begin
 		if(~rst_ni) begin
 			issue_q <= '0;
+			has_flushed_q <= '0;
 		end else begin
 			issue_q <= issue_n;
+			has_flushed_q <= has_flushed_n;
 		end
 	end
 
